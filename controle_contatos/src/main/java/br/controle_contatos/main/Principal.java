b4bda33ac757c56afd102a1e3c5ad774775ab2de
Gabriel-mDias <gabrielmagdias@gmail.com>
2021-06-06 20:15:58 -0300

    Novas views

--- controle_contatos/src/main/java/br/controle_contatos/main/Principal.java
+++ controle_contatos/src/main/java/br/controle_contatos/main/Principal.java
@@ -5,6 +5,7 @@
  */
 package br.controle_contatos.main;
 
+import br.controle_contatos.business.ClienteBusiness;
 import br.controle_contatos.dao.ClienteDAO;
 import br.controle_contatos.models.Cliente;
 
@@ -19,7 +20,7 @@ public class Principal {
         try{
             System.out.println("Resultado da consulta: ");
             
-            for(Cliente elemento : new ClienteDAO().getByParametros(null)){
+            for(Cliente elemento : new ClienteBusiness().getByParametros(new Cliente())){
                 System.out.println(elemento.toString());
             }
             
